ce51c21d4de776b5c264633f513872a79955ca3e
asil <asil.yihong@gmail.com>
2013-12-25 22:58:34 +0800

    Add 2 SVR to control Setpoint and conkP.
    1. Add 2 SVR to control Setpoint and conkP.
    2. Change the PID library.

--- PID_VertiBOT_V3_2.ino
+++ PID_VertiBOT_V3_2.ino
@@ -17,25 +17,27 @@
 // Ocero El Bierzo, Mayo'13
 
  ********************************************************/
-
-//#include <PID_v1.h>
+#include <PID_v1.h>
 
 #include <SoftwareSerial.h>
-#include <PololuQik.h>    
+//#include <PololuQik.h>
 #include <FreeSixIMU.h>
 #include <FIMU_ADXL345.h>
 #include <FIMU_ITG3200.h>
 #include <Wire.h>
 
-#define BUTTON_INPUT  2
-#define MOTOR_CLOCKW  7
-#define MOTOR_CCLOCKW 8
+#define SETPOINT_INPUT   A0
+#define PID_P_INPUT      A1
+#define LED_OUTPUT       13
+
+#define MOTOR_CLOCKW  8
+#define MOTOR_CCLOCKW 7
 #define SKIP_INTERVAL 0
+#define MOTOR_MIN     159
 
 const int Motor_E1 = 5; // digital pin 5 of Arduino (PWM)
 const int Motor_E2 = 6; // digital pin 6 of Arduino (PWM)
 
-#define BUTTON_INPUT 2
 int Motor_M1;
 int Motor_M2;
 // 此兩個變數用來定義馬達正反轉的腳位，要做動態調整所以不定義初始值
@@ -44,14 +46,15 @@ int motorSpeed = 0;
 // 這一個變數用來控制馬達轉速(pwm電壓)
 
 // Define Variables we'll be connecting to
-double Setpoint, Input, Output;
+double Setpoint = 90, Input = 0, Output = 0;
 // Aggressive
 double aggK=1, aggKp=50, aggKi=1.5, aggKd=-8; 
 // Conservative
-double consK=1, consKp=15, consKi=0.005, consKd=3;
+// double consK=1, consKp=15, consKi=0.005, consKd=3;
+double consK=1, consKp=8, consKi=0.005, consKd=3;
 // Specify the links and initial tuning parameters
-// PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
-PololuQik2s9v1 qik(2, 3, 4);
+PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
+//PololuQik2s9v1 qik(2, 3, 4);
 unsigned long serialTime=0; //this will help us know when to talk with processing
 float angles[3]; // yaw pitch roll
 // Set the FreeSixIMU object
@@ -62,26 +65,51 @@ int lastLoopTime = STD_LOOP_TIME;
 int lastLoopUsefulTime = STD_LOOP_TIME;
 unsigned long loopStartTime = 0;
 int a = 1;
-int buttonState = 0;
+double gap = 0;
+
+double dMap(double val, double inLow, double inHigh, double outLow, double outHigh) {
+    double oL = min(outLow, outHigh), oH = max(outLow, outHigh);
+    double iL = min(inLow, inHigh), iH = max(inLow, inHigh);
+    return (oL + (val - iL) * (oH -oL) / (iH - iL));
+}
 
 void setup() {
+    int setpointIn = 0, pidPIn = 0;
     Serial.begin(9600);
-    Setpoint = 97;
-    qik.init();
+    pinMode(SETPOINT_INPUT, INPUT);
+    pinMode(PID_P_INPUT, INPUT);
+    pinMode(LED_OUTPUT, OUTPUT);
+//    qik.init();
+    myPID.SetOutputLimits(-127, 127); //[-127, 127]//tell the PID to range between motor PWM response
+    myPID.SetSampleTime(10);  // Ensure that the PID is called at regular interval  
+    myPID.SetMode(AUTOMATIC);
+    myPID.SetControllerDirection(DIRECT);
     Wire.begin();  
     delay(1000);
     sixDOF.init(); //begin the IMU 
     
-    Serial.println("setup 1");
-    pinMode(BUTTON_INPUT, INPUT);
-    int btnState = LOW;
+    setpointIn = analogRead(SETPOINT_INPUT);
+    pidPIn = analogRead(PID_P_INPUT);
     
-    while((btnState = digitalRead(BUTTON_INPUT)) == LOW) {
-        sixDOF.getEuler(angles);
-        Setpoint=abs(angles[2]);
-        Serial.print("Setpoint: ");
-        Serial.println(Setpoint);
-    }
+    Setpoint = 95 + dMap(setpointIn, 20, 600, -5, 5);
+    consKp = 7 + map(pidPIn, 20, 600, -5, 5);
+    
+    Serial.print("setpointIn: ");
+    Serial.println(setpointIn);    
+    Serial.print("Setpoint: ");
+    Serial.println(Setpoint);
+    Serial.print("pidPIn: ");
+    Serial.println(pidPIn);
+    Serial.print("consKp: ");
+    Serial.println(consKp);
+    
+    digitalWrite(LED_OUTPUT, HIGH);
+    delay(100);
+    digitalWrite(LED_OUTPUT, LOW);
+    delay(100);
+    digitalWrite(LED_OUTPUT, HIGH);
+    delay(100);
+    digitalWrite(LED_OUTPUT, LOW);
 }
 
 void loop() {
@@ -89,34 +117,35 @@ void loop() {
     Input = abs(angles[2]);
     /* Change the tuning parameters: use Conservative Tuning Parameters when we're near
        setpoint and more agressive Tuning Parameters when we're farther away */ 
-    double gap = abs(Setpoint - Input); // distance away from setpoint
+    gap = abs(Setpoint - Input); // distance away from setpoint
     if (gap < 10) { 
         // we're close to setpoint, use conservative tuning parameters
-        // myPID.SetTunings(consKp, consKi, consKd);
-        Output = updatePid(Setpoint, Input, consK, consKp, consKi, consKd);
+        myPID.SetTunings(consKp, consKi, consKd);
+//        Output = updatePid(Setpoint, Input, consK, consKp, consKi, consKd);
     } else {
         // we're far from setpoint, use aggressive tuning parameters
-        // myPID.SetTunings(aggKp, aggKi, aggKd);
-        Output = updatePid(Setpoint, Input, aggK, aggKp, aggKi, aggKd);
+        myPID.SetTunings(aggKp, aggKi, aggKd);
+//        Output = updatePid(Setpoint, Input, aggK, aggKp, aggKi, aggKd);
     }
 
+    myPID.Compute();
     if (Input < 15) {    
         Output = 0; 
     }
 
     if (Output > SKIP_INTERVAL) {
-        motorSpeed = map(Output, 20, 127, 200, 255);    // 則motorSpeed(馬達轉速) 以map函數以0~400的區域轉換成255~110
+        motorSpeed = map(Output, SKIP_INTERVAL, 127, MOTOR_MIN, 255);    // 則motorSpeed(馬達轉速) 以map函數以0~400的區域轉換成255~110
         Motor_M1 = MOTOR_CLOCKW;                        // 這兩行代表的是一個旋轉方向，數字對調就是另一個
         Motor_M2 = MOTOR_CCLOCKW;
     } else if (Output < -SKIP_INTERVAL) {
-        motorSpeed = map(Output, -127, -20, 200, 255);
+        motorSpeed = map(Output, -127, -SKIP_INTERVAL, MOTOR_MIN, 255);
         Motor_M1 = MOTOR_CCLOCKW;                       // 這兩行代表的是一個旋轉方向，數字對調就是另一個
         Motor_M2 = MOTOR_CLOCKW;
     } else {
         motorSpeed = 0;
     }
 
-    qik.setSpeeds(Output, Output);
+//    qik.setSpeeds(Output, Output);
     // asil add
     digitalWrite(Motor_M1, HIGH);       // 數位寫入馬達正反轉
     analogWrite(Motor_E1, motorSpeed);  // 類比寫入馬達轉速
@@ -124,6 +153,7 @@ void loop() {
     analogWrite(Motor_E2, motorSpeed);  // 類比寫入馬達轉速
 
     if(millis() > serialTime) {
+//        SerialReceive();
         SerialSend();
         serialTime += 500;
     }
